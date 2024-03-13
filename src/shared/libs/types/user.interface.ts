8179c45bd488816bb49979567a3776783a24b32a
Rinat <r.z.zaripov@gmail.com>
2024-03-13 12:15:58 +0300

    feat: добавил типы для авторизации токеном

--- src/shared/libs/types/user.interface.ts
+++ src/shared/libs/types/user.interface.ts
@@ -1,8 +1,29 @@
-import { BaseUser } from './base-user.interface';
+import { UserRole } from './user-role.enum';
+import { Location } from './location.enum';
+import { Level } from './level.enum';
+import { TrainType } from './train-type.enum';
+import { Gender } from './gender.enum';
 import { Duration } from './duration.enum';
 
-export interface User extends BaseUser {
-  duration: Duration;
-  caloriesTarget: number;
-  caloriesDaily: number;
+export interface User {
+  id?: string;
+  name: string;
+  email: string;
+  avatar: string;
+  passwordHash: string;
+  gender: Gender;
+  birthDate?: Date;
+  role: UserRole;
+  description: string;
+  location: Location;
+  backgroundImage: string;
+  createdAt: Date;
+  level: Level;
+  trainType: TrainType;
+  isReadyTrain: boolean;
+  duration?: Duration;
+  caloriesTarget?: number;
+  caloriesDaily?: number;
+  certificates?: string;
+  achievements?: string;
 }

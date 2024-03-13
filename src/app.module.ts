67cd93ef6cb23a1d85f682dd376dbd23bd014efd
Rinat <r.z.zaripov@gmail.com>
2024-03-13 12:21:49 +0300

    feat: added guards for authorization

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {
+  ConfigBackendModule,
+  getMongoOptions,
+  getJwtOptions,
+} from './shared/libs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigBackendModule,
+    MongooseModule.forRootAsync(getMongoOptions()),
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: getJwtOptions,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

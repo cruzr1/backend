10e1339774c769290e1f5ecf5599ad8ce9c10e21
Rinat <r.z.zaripov@gmail.com>
2024-03-13 12:20:12 +0300

    feat: добавил различные стратегии авторизации

---
+++ src/strategies/jwt-refresh.strategy.ts
@@ -0,0 +1,37 @@
+import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { jwtConfig } from 'src/shared/libs/config';
+import { UserService } from '../users/users.service';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { RefreshTokenPayload } from 'src/shared/libs/types';
+import { RefreshTokenService } from '../refresh-token/refresh-token.service';
+import { REFRESH_TOKEN_NOT_EXISTS } from '../users/users.constant';
+import { UserEntity } from '../users/user.entity';
+
+@Injectable()
+export class JwtRefreshStrategy extends PassportStrategy(
+  Strategy,
+  'jwt-refresh',
+) {
+  constructor(
+    @Inject(jwtConfig.KEY)
+    private readonly jwtOptions: ConfigType<typeof jwtConfig>,
+    private readonly userService: UserService,
+    private readonly refreshTokenService: RefreshTokenService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: jwtOptions.refreshTokenSecret,
+    });
+  }
+
+  public async validate(payload: RefreshTokenPayload): Promise<UserEntity> {
+    if (!(await this.refreshTokenService.isExists(payload.tokenId))) {
+      throw new UnauthorizedException(REFRESH_TOKEN_NOT_EXISTS);
+    }
+    await this.refreshTokenService.deleteRefreshSession(payload.tokenId);
+    await this.refreshTokenService.deleteExpiredRefreshTokens();
+    return await this.userService.getUserByEmail(payload.email);
+  }
+}
---
+++ src/strategies/local.strategy.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-local';
+import { UserService } from '../users/users.service';
+import { USERMAIL_FIELD } from '../users/users.constant';
+import { User } from 'src/shared/libs/types';
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly userService: UserService) {
+    super({ usernameField: USERMAIL_FIELD });
+  }
+
+  public async validate(email: string, password: string): Promise<User> {
+    return (await this.userService.verifyUser({ email, password })).toPOJO();
+  }
+}

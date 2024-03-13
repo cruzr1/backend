50521b114ebda78d706647f746129e969efd5fd2
Rinat <r.z.zaripov@gmail.com>
2024-03-13 12:41:27 +0300

    feat: added Swagger documentation for the app, enabled cors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,31 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 const DEFAULT_PORT = 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || DEFAULT_PORT);
+  app.enableCors();
+  const config = new DocumentBuilder()
+    .setTitle('The Fit Friends application service')
+    .setDescription('Fit Friends service API')
+    .setVersion('1.0')
+    .build();
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('spec', app, document);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
+  const port = process.env.PORT || DEFAULT_PORT;
+  await app.listen(port);
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
+  );
 }
 bootstrap();

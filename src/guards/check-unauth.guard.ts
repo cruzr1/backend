7f0b78bf8c094b07c383e5b1ba4c6a2543c0db57
Rinat <r.z.zaripov@gmail.com>
2024-03-14 10:53:21 +0300

    feat: added guard to check for anonimization

---
+++ src/guards/check-unauth.guard.ts
@@ -0,0 +1,19 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+
+@Injectable()
+export class CheckUnAuthGuard implements CanActivate {
+  public async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    if (request.headers['authorization']) {
+      throw new ForbiddenException({
+        Authorization: request.headers['authorization'],
+      });
+    }
+    return true;
+  }
+}

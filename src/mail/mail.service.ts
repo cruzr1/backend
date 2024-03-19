3482bc96a2cec2539a37f349b8a098e6d974488b
Rinat <r.z.zaripov@gmail.com>
2024-03-19 17:11:14 +0300

    feat: added sending emails about new trainings

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,9 +1,15 @@
 import { Injectable, Inject } from '@nestjs/common';
 import { MailerService } from '@nestjs-modules/mailer';
-import { EMAIL_ADD_USER_SUBJECT, USER_TEMPLATE_PATH } from './mail.constant';
+import {
+  EMAIL_ADD_USER_SUBJECT,
+  USER_TEMPLATE_PATH,
+  NEW_TRAINING_SUBJECT,
+  NEW_TRAINING_TEMPLATE_PATH,
+} from './mail.constant';
 import { ConfigType } from '@nestjs/config';
 import { CreateUserDto } from '../users/dto/create-user.dto';
 import { MailerConfig } from 'src/shared/libs/config';
+import { Training } from 'src/shared/libs/types';
 
 @Injectable()
 export class MailService {
@@ -17,12 +23,54 @@ export class MailService {
     await this.mailerService.sendMail({
       from: this.mailerConfig.mailFrom,
       to: email,
+
       subject: EMAIL_ADD_USER_SUBJECT,
       template: USER_TEMPLATE_PATH,
       context: {
         email: `${email}`,
         name: `${name}`,
         password: `${password}`,
+      },
+    });
+  }
+
+  public async sendNotifyNewTraining(
+    {
+      name: trainName,
+      level,
+      trainType,
+      duration,
+      price,
+      calories,
+      description,
+      gender,
+      rating,
+      trainerId,
+      isSpecial,
+      videoURL,
+    }: Training,
+    name: string,
+    email: string,
+  ) {
+    await this.mailerService.sendMail({
+      from: this.mailerConfig.mailFrom,
+      to: email,
+      subject: NEW_TRAINING_SUBJECT,
+      template: NEW_TRAINING_TEMPLATE_PATH,
+      context: {
+        trainName: `${trainName}`,
+        level: `${level}`,
+        trainType: `${trainType}`,
+        duration: `${duration}`,
+        price: `${price}`,
+        description: `${description}`,
+        calories: `${calories}`,
+        gender: `${gender}`,
+        rating: `${rating}`,
+        trainerId: `${trainerId}`,
+        isSpecial: `${isSpecial}`,
+        videoURL: `${videoURL}`,
+        name: `${name}`,
       },
     });
   }
